1fc08095a837f2b7095c2162afd0fec007d29db4
Im-perfect <zhangke.kerri@gmail.com>
2019-11-12 16:19:06 +0100

    go back button with router

---
+++ src/components/GoBackButton.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import { withRouter } from "react-router";
+
+export class GoBackButton extends Component {
+  handleClick = () => {
+      this.props.history.push(this.props.path)
+  };
+  render() {
+    return (
+      <div>
+        <button onClick={this.handleClick}>Previous step</button>
+      </div>
+    );
+  }
+}
+
+export default withRouter(GoBackButton);
