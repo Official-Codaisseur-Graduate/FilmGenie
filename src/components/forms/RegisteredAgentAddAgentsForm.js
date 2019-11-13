cb291b401c4be75633940775fff46a4d3415c4d6
Im-perfect <zhangke.kerri@gmail.com>
2019-11-13 18:04:38 +0100

    fix

--- src/components/forms/RegisteredAgentAddAgentsForm.js
+++ src/components/forms/RegisteredAgentAddAgentsForm.js
@@ -21,67 +21,78 @@ export default class RegisteredAgentAddAgentsForm extends Component {
 
   render() {
     const { type } = this.props;
-    const nameAgent = agentType(type);
+    const nameAgent = this.agentType(type);
 
     return (
       <div>
+        <h4>
+          <em>{type}</em>
+        </h4>
         <form>
           <div>
-
             {/* In administator there should be filled in data */}
-            <label htmlFor="name">{type}</label>
             <label htmlFor="name">{nameAgent}</label>
 
-              <input type="text" name="agent_type" placeholder="Name"></input>
+            <input type="text" name="agent_type" placeholder="Name"></input>
             <label htmlFor="administrator">Administrator</label>
-             <input type="text" name="full_name" placeholder="Full Name"></input>
+            <input type="text" name="full_name" placeholder="Full Name"></input>
             <label htmlFor="firstname"></label>
-              <input type="text" name="firstname" placeholder="First Name"></input>
+            <input
+              type="text"
+              name="firstname"
+              placeholder="First Name"
+            ></input>
             <label htmlFor="lastname"></label>
-              <input type="text" name="lastname" placeholder="Last Name"></input>
+            <input type="text" name="lastname" placeholder="Last Name"></input>
             <label htmlFor="job"></label>
-             <input type="text" name="job" placeholder="Job"></input>
+            <input type="text" name="job" placeholder="Job"></input>
           </div>
 
           <button>plus_sign</button>
 
           <div>
-          <label htmlFor="editor">Editor</label>
-             <input type="text" name="full_name" placeholder="Full Name"></input>
+            <label htmlFor="editor">Editor</label>
+            <input type="text" name="full_name" placeholder="Full Name"></input>
             <label htmlFor="firstname"></label>
-              <input type="text" name="firstname" placeholder="First Name"></input>
+            <input
+              type="text"
+              name="firstname"
+              placeholder="First Name"
+            ></input>
             <label htmlFor="lastname"></label>
-              <input type="text" name="lastname" placeholder="Last Name"></input>
+            <input type="text" name="lastname" placeholder="Last Name"></input>
             <label htmlFor="job"></label>
-             <input type="text" name="job" placeholder="Job"></input>
+            <input type="text" name="job" placeholder="Job"></input>
           </div>
 
           <button>plus_sign</button>
 
           <div>
-          <label htmlFor="editor">Reader</label>
-             <input type="text" name="full_name" placeholder="Full Name"></input>
+            <label htmlFor="editor">Reader</label>
+            <input type="text" name="full_name" placeholder="Full Name"></input>
             <label htmlFor="firstname"></label>
-              <input type="text" name="firstname" placeholder="First Name"></input>
+            <input
+              type="text"
+              name="firstname"
+              placeholder="First Name"
+            ></input>
             <label htmlFor="lastname"></label>
-              <input type="text" name="lastname" placeholder="Last Name"></input>
+            <input type="text" name="lastname" placeholder="Last Name"></input>
             <label htmlFor="job"></label>
-             <input type="text" name="job" placeholder="Job"></input>
+            <input type="text" name="job" placeholder="Job"></input>
           </div>
-
         </form>
       </div>
     );
   }
 }
 
-
 // const mapStateToProps = (state) => ({
-    
+
 // })
 
 // const mapDispatchToProps = {
-    
+
 // }
 
 // export default connect(mapStateToProps, mapDispatchToProps)(RegisteredAgentAddAgentsFor)
