61cde96d6e18ec48178e3dc23510392fce0d4c17
marloesverdonk <marloesverdonk@live.nl>
2019-11-13 14:45:55 +0100

    changed route names

--- src/Routes.js
+++ src/Routes.js
@@ -32,7 +32,7 @@ import CinemaGroupCinemas from './components/pages/filmAgent/registeredAgent/cin
 import CinemaGroupDetails from './components/pages/filmAgent/registeredAgent/cinemaGroup/CinemaGroupDetails'
 // pages/filmAgent/registeredAgent/filmFestival
 import FilmFestival from './components/pages/filmAgent/registeredAgent/filmFestival/FilmFestival'
-import FilmFestivalAgents from './components/pages/filmAgent/registeredAgent/filmFestival/FilmFestival'
+import FilmFestivalAgents from './components/pages/filmAgent/registeredAgent/filmFestival/FilmFestivalAgents'
 import FilmFestivalDetails from './components/pages/filmAgent/registeredAgent/filmFestival/FilmFestivalDetails'
 import FilmFestivalEditions from './components/pages/filmAgent/registeredAgent/filmFestival/FilmFestivalEditions'
 import FilmFestivalScreenings from './components/pages/filmAgent/registeredAgent/filmFestival/FilmFestivalScreenings'
@@ -60,46 +60,46 @@ import Tickets from './components/pages/cinemaGoer/registeredCinemaGoer/filmScre
 
 function Routes() {
   return (<div>
-    <Route path="/1" exact component={Start} />
-    <Route path="/2" exact component={FilmAgentRegistration} />
-    <Route path="/3" exact component={CinemaGoerRegistration} />
-    <Route path="/4" exact component={NewFilmAgent} />
-    <Route path="/5" exact component={NewFilmExhibitor} />
-    <Route path="/6" exact component={RegisteredFilmAgent} />
-    <Route path="/7" exact component={RegisteredCinemaGoer} />
-    <Route path="/8" exact component={CinemaGoer} />
-    <Route path="/9" exact component={NewRightOwner} />
-    <Route path="/10" exact component={NewFilmFestival} />
-    <Route path="/11" exact component={NewCinemaOrCinemaGroup} />
-    <Route path="/12" exact component={NewCinemaGroup} />
-    <Route path="/13" exact component={NewCinema} />
-    <Route path="/14" exact component={FilmFestival} />
-    <Route path="/15" exact component={FilmFestivalAgents} />
-    <Route path="/16" exact component={FilmFestivalEditions} />
-    <Route path="/17" exact component={CinemaGroup} />
-    <Route path="/18" exact component={CinemaGroupAgents} />
-    <Route path="/19" exact component={CinemaGroupCinemas} />
-    <Route path="/20" exact component={Cinema} />
-    <Route path="/21" exact component={CinemaAgents} />
-    <Route path="/22" exact component={CinemaProgram} />
-    <Route path="/23" exact component={RightOwner} />
-    <Route path="/24" exact component={RightOwnerAgents} />
-    <Route path="/25" exact component={RightOwnerFilms} />
-    <Route path="/26" exact component={RightOwnerDetails} />
-    <Route path="/27" exact component={FilmFestivalDetails} />
-    <Route path="/28" exact component={CinemaGroupDetails} />
-    <Route path="/29" exact component={CinemaDetails} />
-    <Route path="/30" exact component={CinemaHalls} />
-    <Route path="/31" exact component={CinemaTicketTypes} />
-    <Route path="/32" exact component={Screenings} />
-    <Route path="/33" exact component={FilmFestivalScreenings} />
-    <Route path="/34" exact component={NewCinemaGoer} />
-    <Route path="/35" exact component={Tickets} />
-    <Route path="/36" exact component={ScreeningsOfSelectedFilm} />
-    <Route path="/37" exact component={Films} />
-    <Route path="/38" exact component={AccessControl} />
-    <Route path="/39" exact component={Ticket} />
-    <Route path="/40" exact component={Film} />
+    <Route path="/Start" exact component={Start} /> 
+    <Route path="/filmAgent/FilmAgentRegistration" exact component={FilmAgentRegistration} />
+    <Route path="/cinemaGoer/CinemaGoerRegistration" exact component={CinemaGoerRegistration} />
+    <Route path="/filmAgent/newAgent/NewFilmAgent" exact component={NewFilmAgent} />
+    <Route path="/filmAgent/newAgent/NewFilmExhibitor" exact component={NewFilmExhibitor} />
+    <Route path="/filmAgent/registeredAgent/RegisteredFilmAgent" exact component={RegisteredFilmAgent} />
+    <Route path="/cinemaGoer/registeredCinemaGoer/RegisteredCinemaGoer" exact component={RegisteredCinemaGoer} />
+    <Route path="/cinemaGoer/registeredCinemaGoer/CinemaGoer" exact component={CinemaGoer} />
+    <Route path="/filmAgent/newAgent/NewRightOwner" exact component={NewRightOwner} />
+    <Route path="/filmAgent/newAgent/NewFilmFestival" exact component={NewFilmFestival} />
+    <Route path="/filmAgent/newAgent/NewCinemaOrCinemaGroup" exact component={NewCinemaOrCinemaGroup} />
+    <Route path="/filmAgent/newAgent/NewCinemaGroup" exact component={NewCinemaGroup} />
+    <Route path="/filmAgent/newAgent/NewCinema" exact component={NewCinema} />
+    <Route path="/filmAgent/registeredAgent/filmFestival/FilmFestival" exact component={FilmFestival} />
+    <Route path="/filmAgent/registeredAgent/filmFestival/FilmFestivalAgents" exact component={FilmFestivalAgents} />
+    <Route path="/filmAgent/registeredAgent/filmFestival/FilmFestivalEditions" exact component={FilmFestivalEditions} />
+    <Route path="/filmAgent/registeredAgent/cinemaGroup/CinemaGroup" exact component={CinemaGroup} />
+    <Route path="/filmAgent/registeredAgent/cinemaGroup/CinemaGroupAgents" exact component={CinemaGroupAgents} />
+    <Route path="/filmAgent/registeredAgent/cinemaGroup/CinemaGroupCinemas" exact component={CinemaGroupCinemas} />
+    <Route path="/filmAgent/registeredAgent/cinema/Cinema" exact component={Cinema} />
+    <Route path="/filmAgent/registeredAgent/cinema/CinemaAgents" exact component={CinemaAgents} />
+    <Route path="/filmAgent/registeredAgent/cinema/CinemaProgram" exact component={CinemaProgram} />
+    <Route path="/filmAgent/registeredAgent/rightOwner/RightOwner" exact component={RightOwner} />
+    <Route path="/filmAgent/registeredAgent/rightOwner/RightOwnerAgents" exact component={RightOwnerAgents} />
+    <Route path="/filmAgent/registeredAgent/rightOwner/RightOwnerFilms" exact component={RightOwnerFilms} />
+    <Route path="/filmAgent/registeredAgent/rightOwner/RightOwnerDetails" exact component={RightOwnerDetails} />
+    <Route path="/filmAgent/registeredAgent/filmFestival/FilmFestivalDetails" exact component={FilmFestivalDetails} />
+    <Route path="/filmAgent/registeredAgent/cinemaGroup/CinemaGroupDetails" exact component={CinemaGroupDetails} />
+    <Route path="/filmAgent/registeredAgent/cinema/CinemaDetails" exact component={CinemaDetails} />
+    <Route path="/filmAgent/registeredAgent/cinema/CinemaHalls" exact component={CinemaHalls} />
+    <Route path="/filmAgent/registeredAgent/cinema/CinemaTicketTypes" exact component={CinemaTicketTypes} />
+    <Route path="/cinemaGoer/registeredCinemaGoer/filmScreenings/Screenings" exact component={Screenings} />
+    <Route path="/filmAgent/registeredAgent/filmFestival/FilmFestivalScreenings" exact component={FilmFestivalScreenings} />
+    <Route path="/cinemaGoer/newCinemaGoer/NewCinemaGoer" exact component={NewCinemaGoer} />
+    <Route path="/cinemaGoer/registeredCinemaGoer/filmScreenings/Tickets" exact component={Tickets} />
+    <Route path="/cinemaGoer/registeredCinemaGoer/films/ScreeningsOfSelectedFilm" exact component={ScreeningsOfSelectedFilm} />
+    <Route path="/cinemaGoer/registeredCinemaGoer/films/Films" exact component={Films} />
+    <Route path="/filmAgent/AccessControl" exact component={AccessControl} />
+    <Route path="/cinemaGoer/Ticket" exact component={Ticket} />
+    <Route path="/Film" exact component={Film} />
   </div>);
 }
 
