965d16895e50f4672c9ea386a7473d3cd89a6894
Im-perfect <zhangke.kerri@gmail.com>
2019-11-13 15:17:43 +0100

    routing page 2,3

--- src/components/pages/Start.js
+++ src/components/pages/Start.js
@@ -1,11 +1,32 @@
 //page 1
 
-import React from "react"
+import React from "react";
+import ChoiceButtons from "../UI/ChoiceButtons";
 
 export default class Start extends React.Component {
-  render() {
-    return (<div>
-      Start
-    </div>)
+  filmAgent = () =>{
+    this.props.history.push("/2")
   }
-} 
+
+  cinemaGoer = () => {
+    this.props.history.push("/3")
+  }
+
+  render() {
+    const choice1 = {
+      name: "I am a film agent",
+      action: this.filmAgent
+    }
+
+    const choice2 = {
+      name: "I am a cinema-goer",
+      action: this.cinemaGoer
+    }
+
+    return (
+      <div>
+        <ChoiceButtons choice1={choice1} choice2={choice2} />
+      </div>
+    );
+  }
+}
