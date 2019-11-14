39f0c4ead08fd6f78a7c2c611976055fa5c88154
Im-perfect <zhangke.kerri@gmail.com>
2019-11-14 13:22:23 +0100

    new cinema goer form

---
+++ src/components/forms/cinemaGoer/NewCinemaGoerForm.js
@@ -0,0 +1,53 @@
+//page 34
+
+import React, { Component } from "react";
+// import { connect } from 'react-redux'
+import { withRouter } from "react-router-dom";
+
+class NewCinemaGoerForm extends Component {
+  handleSubmit = event => {
+    event.preventDefault();
+
+    this.props.history.push(
+      "/cinemaGoer/registeredCinemaGoer/RegisteredCinemaGoer"
+    );
+  };
+
+  render() {
+    return (
+      <div>
+        <h4>
+          <em>New cinema-goer</em>
+        </h4>
+        <form onSubmit={this.handleSubmit}>
+          <h5>Film consumer</h5>
+          <label htmlFor="fullname">Full name</label>
+          <input type="text" name="fullname" placeholder="Full name"></input>
+          <label htmlFor="city">City</label>
+          <input type="text" name="city" placeholder="City"></input>
+          <label htmlFor="email">E-mail address</label>
+          <input type="email" name="email" placeholder="E-mail address"></input>
+          <label htmlFor="passoword">Password</label>
+          <input
+            type="password"
+            name="passoword"
+            placeholder="Password"
+          ></input>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+// const mapStateToProps = (state) => ({
+
+// })
+
+// const mapDispatchToProps = {
+
+// }
+
+// export default connect(mapStateToProps, mapDispatchToProps)(NewCinemaGoerForm)
+
+export default withRouter(NewCinemaGoerForm);
