de178d51ae599207eede67749451c9253af2cb0d
Im-perfect <zhangke.kerri@gmail.com>
2019-11-15 15:17:26 +0100

    add go back buttons

--- src/components/pages/cinemaGoer/newCinemaGoer/NewCinemaGoer.js
+++ src/components/pages/cinemaGoer/newCinemaGoer/NewCinemaGoer.js
@@ -2,11 +2,14 @@
 
 import React from "react";
 import NewCinemaGoerForm from "../../../forms/cinemaGoer/NewCinemaGoerForm";
+import GoBackButton from "../../../UI/GoBackButton";
 
 export default class NewCinemaGoer extends React.Component {
   render() {
     return (
       <div>
+        <GoBackButton path="/cinemaGoer/CinemaGoerRegistration" />
+
         <NewCinemaGoerForm />
       </div>
     );
