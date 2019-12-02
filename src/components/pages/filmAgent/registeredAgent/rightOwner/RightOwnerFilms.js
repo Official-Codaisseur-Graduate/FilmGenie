b4667d2dfc5338f3d9f728b8b3008c86505e8fc3
ahaigh9877 <alastairhaigh@me.com>
2019-12-02 17:12:26 +0100

    test commit

--- src/components/pages/filmAgent/registeredAgent/rightOwner/RightOwnerFilms.js
+++ src/components/pages/filmAgent/registeredAgent/rightOwner/RightOwnerFilms.js
@@ -1,33 +1,57 @@
 //page 25
 
-import React from "react"
+import React from "react";
 import GoBackSideButton from "../../../../UI/GoBackSideButton";
-import RightOwnerFilmsForm from "../../../../forms/rightOwner/RightOwnerFilmsForm"
+import RightOwnerFilmsForm from "../../../../forms/rightOwner/RightOwnerFilmsForm";
 
 export default class RightOwnerFilms extends React.Component {
   state = {
-    films: [{ title: null, imdbNumber: null, territory: null, startDate: null, endDate: null }]
-  }
-  
+    films: [
+      {
+        title: null,
+        imdbNumber: null,
+        territory: null,
+        startDate: null,
+        endDate: null
+      }
+    ]
+  };
+
   details = () => {
-    this.props.history.push("/Film") //page 40
-  }
+    this.props.history.push("/Film"); //page 40
+  };
 
   addFilm = () => {
     this.setState({
-      films: [...this.state.films, { title: null, imdbNumber: null, Territory: null, startDate: null, endDate: null }]
-    })
-  }
-  
+      films: [
+        ...this.state.films,
+        {
+          title: null,
+          imdbNumber: null,
+          Territory: null,
+          startDate: null,
+          endDate: null
+        }
+      ]
+    });
+  };
+
   render() {
-    const { films } = this.state
-    return (<div>
-      <GoBackSideButton path="/filmAgent/registeredAgent/rightOwner/RightOwner" />
-      <h3>Right owner films</h3>
-      {films.map((film,index) => {
-        return <div><p>Film {index +1}</p><RightOwnerFilmsForm details={this.details}/></div>
-      })}
-      <button onClick={this.addFilm}>+</button>
-    </div>)
+    const { films } = this.state;
+    return (
+      <div>
+        <GoBackSideButton path="/filmAgent/registeredAgent/rightOwner/RightOwner" />
+        <h3>Right owner films</h3>
+        {films.map((film, index) => {
+          return (
+            <div>
+              <p>Film {index + 1}</p>
+              <RightOwnerFilmsForm details={this.details} />
+            </div>
+          );
+        })}
+        <button onClick={this.addFilm}> ADD FILM</button>
+      </div>
+    );
   }
-}  
+}
